9261952ac13d34b0a93ca8fccf69cad7c3d433b7
Lucas Almeida <lucas_almeidasilva01@hotmail.com>
2021-01-25 13:45:02 -0300

    Adding meta tags for SEO

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import db from '../../db.json';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -26,5 +27,47 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+
+          <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
+          <meta name="MobileOptimized" content="320" />
+          <meta name="HandheldFriendly" content="True" />
+          <meta name="theme-color" content={db.primary} />
+          <meta name="msapplication-TileColor" content={db.primary} />
+          <meta name="referrer" content="no-referrer-when-downgrade" />
+          <meta name="google" content="notranslate" />
+
+          <meta property="og:title" content="Quiz Marvel" />
+          <meta property="og:description" content="Um quiz para fãs do Universo Cinematográfico da Marvel" />
+          <meta property="og:locale" content="pt_BR" />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content="Um quiz para fãs do Universo Cinematográfico da Marvel" />
+          <meta property="og:image" content={db.bg} />
+          <meta property="og:image:secure_url" content={db.bg} />
+          <meta property="og:image:alt" content="Thumbnail" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+
+          <meta name="twitter:title" content="Um quiz para fãs do Universo Cinematográfico da Marvel" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:image" content={db.bg} />
+          <meta name="twitter:image:src" content={db.bg} />
+          <meta name="twitter:image:alt" content="Thumbnail" />
+          <meta name="twitter:image:width" content="1200" />
+          <meta name="twitter:image:height" content="620" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
