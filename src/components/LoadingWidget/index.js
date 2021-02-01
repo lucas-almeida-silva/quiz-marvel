59f414b47fd5db8ea85aab23be6fae6a589d5dc2
Lucas Almeida <lucas_almeidasilva01@hotmail.com>
2021-02-01 00:39:03 -0300

    Adding a loader

--- src/components/LoadingWidget/index.js
+++ src/components/LoadingWidget/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
 import Widget from '../Widget';
+
+const LoaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const loaderRotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Spinner = styled.div`
+  border: 8px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 8px solid ${({ theme }) => theme.colors.primary};
+  width: 50px;
+  height: 50px;
+  animation: ${loaderRotate} 2s linear infinite;
+  --webkit-animation: ${loaderRotate} 2s linear infinite;
+`;
 
 function LoadingWidget() {
   return (
@@ -10,7 +36,9 @@ function LoadingWidget() {
       </Widget.Header>
 
       <Widget.Content>
-        Aguarde
+        <LoaderWrapper>
+          <Spinner />
+        </LoaderWrapper>
       </Widget.Content>
     </Widget>
   );
